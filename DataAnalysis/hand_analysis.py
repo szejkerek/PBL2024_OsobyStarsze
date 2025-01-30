587ab47ac96969371387f64e55b68e6ca692d524
Bartek <bartekk.gordon@gmail.com>
2025-01-30 19:39:31 +0100

    More refactor

--- DataAnalysis/hand_analysis.py
+++ DataAnalysis/hand_analysis.py
@@ -1,3 +1,49 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+
+def extract_hand_data(actions, hand):
+    frames = []
+    for action in actions:
+        hand_frames = action.right_hand_frames if hand == "right" else action.left_hand_frames
+        for frame in hand_frames:
+            frames.append({
+                "x": frame.position["x"],
+                "y": frame.position["y"],
+                "z": frame.position["z"],
+                "speed": frame.speed
+            })
+    return pd.DataFrame(frames)
+
+def plot_speed_over_time(actions):
+    right_data = extract_hand_data(actions, "right")
+    left_data = extract_hand_data(actions, "left")
+    
+    plt.figure(figsize=(10, 5))
+    plt.plot(right_data.index, right_data["speed"], label="Right Hand", color="blue")
+    plt.plot(left_data.index, left_data["speed"], label="Left Hand", color="red")
+    plt.xlabel("Frame Index")
+    plt.ylabel("Speed")
+    plt.title("Hand Speed Over Time")
+    plt.legend()
+    plt.show()
+
+def plot_hand_movement_3d(actions, hand):
+    data = extract_hand_data(actions, hand)
+    
+    fig = plt.figure(figsize=(8, 6))
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(data["x"], data["y"], data["z"], c=data.index, cmap='viridis', alpha=0.6)
+    
+    ax.set_xlabel("X Position")
+    ax.set_ylabel("Y Position")
+    ax.set_zlabel("Z Position")
+    ax.set_title(f"3D Movement of {hand.capitalize()} Hand")
+    
+    plt.show()
+
+
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
@@ -42,3 +88,5 @@ def plot_hand_heatmap(actions, hand):
     ax.set_title(f'{hand.capitalize()} Hand Position Heatmap')
     
     plt.show()
+
+
--- DataAnalysis/main.py
+++ DataAnalysis/main.py
@@ -1,6 +1,8 @@
 import json
-from hands_heatmaps import plot_hand_heatmap
 from game_classes import Game 
+from hand_analysis import plot_speed_over_time, plot_hand_movement_3d, plot_hand_heatmap
+from reaction_analysis import plot_reaction_time_distribution, plot_aim_accuracy
+from performance_comparison import compare_hand_performance
 
 file_name = "data.json"
 with open(file_name, "r") as file:
@@ -8,5 +10,18 @@ with open(file_name, "r") as file:
 
 game = Game(file_name, json_data)
 
+# Hand heatmaps
 plot_hand_heatmap(game.actions, "right")
 plot_hand_heatmap(game.actions, "left")
+
+# Hand analysis
+plot_speed_over_time(game.actions)
+plot_hand_movement_3d(game.actions, "right")
+plot_hand_movement_3d(game.actions, "left")
+
+# Reaction and accuracy analysis
+plot_reaction_time_distribution(game.actions)
+plot_aim_accuracy(game.actions)
+
+# Performance comparison
+compare_hand_performance(game.actions)
